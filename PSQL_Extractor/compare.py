353e56593f746f979dfa3035466e73aebc66caa0
Shantanu Sinha <shantanu.p.sinha@gmail.com>
2023-11-07 14:14:40 -0500

    compare

--- PSQL_Extractor/compare.py
+++ PSQL_Extractor/compare.py
@@ -1,29 +1,32 @@
 import json
 import hashlib
 
-# 
-# def compare_ndjson(file1_path, file2_path, output_file1, output_file2):
-#     # Read the data from both files
-#     file1_data = read_ndjson(file1_path)
-#     file2_data = read_ndjson(file2_path)
-# 
-#     # Convert list of dictionaries to list of strings to make comparison faster
-#     file1_data_str = {json.dumps(entry) for entry in file1_data}
-#     file2_data_str = {json.dumps(entry) for entry in file2_data}
-# 
-#     # Find unique entries
-#     unique_to_file1 = file1_data_str - file2_data_str
-#     unique_to_file2 = file2_data_str - file1_data_str
-# 
-#     # Convert back to dictionaries
-#     unique_to_file1 = [json.loads(entry) for entry in unique_to_file1]
-#     unique_to_file2 = [json.loads(entry) for entry in unique_to_file2]
-# 
-#     # Write the unique entries to their respective files
-#     write_ndjson(unique_to_file1, output_file1)
-#     write_ndjson(unique_to_file2, output_file2)
-#     print(f"Unique entries written to {output_file1} and {output_file2}")
-# 
+def read_ndjson(file_path):
+    with open(file_path, 'r') as file:
+        return [json.loads(line) for line in file]
+
+def compare_ndjson(file1_path, file2_path, output_file1, output_file2):
+    # Read the data from both files
+    file1_data = read_ndjson(file1_path)
+    file2_data = read_ndjson(file2_path)
+
+    # Convert list of dictionaries to list of strings to make comparison faster
+    file1_data_str = {json.dumps(entry) for entry in file1_data}
+    file2_data_str = {json.dumps(entry) for entry in file2_data}
+
+    # Find unique entries
+    unique_to_file1 = file1_data_str - file2_data_str
+    unique_to_file2 = file2_data_str - file1_data_str
+
+    # Convert back to dictionaries
+    unique_to_file1 = [json.loads(entry) for entry in unique_to_file1]
+    unique_to_file2 = [json.loads(entry) for entry in unique_to_file2]
+
+    # Write the unique entries to their respective files
+    write_ndjson(unique_to_file1, output_file1)
+    write_ndjson(unique_to_file2, output_file2)
+    print(f"Unique entries written to {output_file1} and {output_file2}")
+
 
 def hash_entry(entry):
     return hashlib.md5(json.dumps(entry, sort_keys=True).encode('utf-8')).hexdigest()
@@ -50,29 +53,29 @@ def write_ndjson(data, file_path):
         for entry in data:
             file.write(json.dumps(entry) + '\n')
 
-def compare_ndjson(file1_path, file2_path, output_file1, output_file2):
-    # Process the first file to get the set of hashes
-    file1_hashes = process_ndjson(file1_path)
-    
-    # Process the second file to find unique entries and get the set of hashes
-    unique_to_file2 = process_ndjson(file2_path, file1_hashes)
-    file2_hashes = process_ndjson(file2_path)
-    
-    # Process the first file again to find unique entries
-    unique_to_file1 = process_ndjson(file1_path, file2_hashes)
-    
-    # Write the unique entries to their respective files
-    write_ndjson(unique_to_file1, output_file1)
-    write_ndjson(unique_to_file2, output_file2)
-    print(f"Unique entries written to {output_file1} and {output_file2}")
+# def compare_ndjson(file1_path, file2_path, output_file1, output_file2):
+#     # Process the first file to get the set of hashes
+#     file1_hashes = process_ndjson(file1_path)
+#     
+#     # Process the second file to find unique entries and get the set of hashes
+#     unique_to_file2 = process_ndjson(file2_path, file1_hashes)
+#     file2_hashes = process_ndjson(file2_path)
+#     
+#     # Process the first file again to find unique entries
+#     unique_to_file1 = process_ndjson(file1_path, file2_hashes)
+#     
+#     # Write the unique entries to their respective files
+#     write_ndjson(unique_to_file1, output_file1)
+#     write_ndjson(unique_to_file2, output_file2)
+#     print(f"Unique entries written to {output_file1} and {output_file2}")
 
 # Replace these with the absolute paths to your files
 file1_path = '/home/shantanu/duality/python-utils/PSQL_Extractor/extracted/npm_packages_100_brokey.ndjson'
 file2_path = '/home/shantanu/duality/python-utils/PSQL_Extractor/extracted/npm_packages_100.ndjson'
 
 # Output files
-output_file1 = 'unique_to_brokey.ndjson'
-output_file2 = 'unique_to_not_brokey.ndjson' # Should be empty
+output_file1 = 'extracted/unique_to_brokey.ndjson'
+output_file2 = 'extracted/unique_to_not_brokey.ndjson' # Should be empty
 
 # Run the comparison
 compare_ndjson(file1_path, file2_path, output_file1, output_file2)
